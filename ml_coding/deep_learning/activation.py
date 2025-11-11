1da138806ba5032834fed6741f1875a3991fbd27
SapphireFeiFei24 <sunxiaofei@sunxiaofeis-MacBook-Air.local>
2025-11-12 00:46:44 +0800

    update

---
+++ ml_coding/deep_learning/activation.py
@@ -0,0 +1,35 @@
+import numpy as np
+
+class ActivationFuncs:
+    def __init__(self):
+        pass
+
+    def sigmoid(self, X):
+        pass
+
+    def sigmoid_grad(self, X):
+        pass
+
+    def tanh(self, X):
+        pass
+
+    def tanh_grad(self, X):
+        pass
+
+    def relu(self, X):
+        pass
+
+    def relu_grad(self, X):
+        pass
+
+    def leaky_relu(self, X):
+        pass
+
+    def leaky_relu_grad(self, X):
+        pass
+
+    def softmax(self, X):
+        pass
+
+    def softmax_grad(self, X):
+        pass
