68293b2787153b496edb3b77aed0818e211eadbf
SapphireFeiFei24 <sunxiaofei@sunxiaofeis-MacBook-Air.local>
2025-11-17 09:55:06 +0800

    update svd

--- ml_coding/traditional_ml_model/svd.py
+++ ml_coding/traditional_ml_model/svd.py
@@ -26,6 +26,7 @@ class SVD:
 
     def truncated_svd(A, k):
         """
+        O(min(mn^2, m^n)) Good for small to medium matrices
         Return U_k, S_k, Vt_k for top-k SVD using numpy
         :param A: (M, N)
         :param k:  <= min(M, N)
@@ -36,3 +37,42 @@ class SVD:
         # so slicing is simple
         U, s, Vt = np.linalg.svd(A, full_matrices=False) #economical SVD
         return U[:, :k], s[:k], Vt[:k, :]
+
+    def top1_svd_power(self, A, num_iters=100, tol=1e-6):
+        """
+        Power method to find top singular triplet (u, sigma, v) of A
+        :param A: (m, n)
+        :param num_iters:
+        :param tol:
+        :return:
+        """
+        m, n = A.shape
+        # init v randomly
+        v = np.random.randn(n)
+        v = v / np.linalg.norm(v)
+        last_sigma = 0.0
+
+        for i in range(num_iters):
+            # w = A^T (A v)
+            Av = A @ v  # (m,)
+            w = A.T @ Av  # (n,)
+            norm_w = np.linalg.norm(w)
+            if norm_w == 0:
+                break
+            v = w / norm_w
+            sigma = np.linalg.norm(Av)
+            if abs(sigma - last_sigma) < tol:
+                break
+            last_sigma = sigma
+
+        # compute u
+        Av = A @ v
+        sigma = np.linalg.norm(Av)
+        if sigma == 0:
+            u = np.zeros(m)
+        else:
+            u = Av / sigma
+        return u, sigma, v
+
+    def topk_svd_subspace(self, A, k, num_iters=20):
+        pass
